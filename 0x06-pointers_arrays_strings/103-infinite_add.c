1cb43e43589e164e514f837dd211c06ffd227842
A-Female-In-Tech <blesshow2019@gmail.com>
2022-09-22 11:59:27 -0700

    Task 11

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -2,10 +2,10 @@ include "main.h"
 
 /**
  * infinite_add - adds two numbers
- *@n1: first number
- *@n2: second number
- *@r: buffer for result
- *@size_r: buffer size
+ * @n1: first number
+ * @n2: second number
+ * @r: buffer for result
+ * @size_r: buffer size
  *
  * Return: address of r or 0
  */
